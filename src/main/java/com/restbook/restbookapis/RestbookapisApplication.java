2929639b020d0f3d46a2c0dba863307b35cff86f
Adnan Rashid <arasheed@teresol.com>
2024-07-04 10:07:25 +0500

    created restbookapis spring boot project

---
+++ src/main/java/com/restbook/restbookapis/RestbookapisApplication.java
@@ -0,0 +1,13 @@
+package com.restbook.restbookapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbookapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbookapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restbook/restbookapis/RestbookapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restbook.restbookapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbookapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
