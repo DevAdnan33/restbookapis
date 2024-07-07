5879b3bc293d8b79c49bb814d0703aad315347ce
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-07 21:08:43 +0500

    added id annotation in entity class

--- src/main/java/com/restbook/restbookapis/entities/Book.java
+++ src/main/java/com/restbook/restbookapis/entities/Book.java
@@ -3,12 +3,14 @@ package com.restbook.restbookapis.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="books")
 public class Book {
 
+    @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
     private String name;
